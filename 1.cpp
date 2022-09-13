ae4c3d886152aa8a57e127a60f66db1d419d4323
90polarbear <100282618+90polarbear@users.noreply.github.com>
2022-09-13 20:22:36 +0800

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,7 @@
 int main(void)  /*int為整數 mian為名稱 void為不指定數字*/
 {
 	float x=10.1, y=20.2;  /*int為整數 x為名稱 x值為10 並開一個記憶體空間給x*/ 
-	float A[10]; /*開10記憶體位址*/
+	float A[10]; /*開10個記憶體位址*/
 	printf("x的值為:%f\n", x);
 	printf("y的值為:%f\n", y);
 	printf("x的位址為:%d\n", &x); /*&記憶體位址*/
