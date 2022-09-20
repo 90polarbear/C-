f8d4e3de7df65fe63c9179f3823d3ba3fba5c57b
90polarbear <100282618+90polarbear@users.noreply.github.com>
2022-09-20 19:52:03 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,10 +4,14 @@ int main(void)  /*int為整數 mian為名稱 void為不指定數字*/
 {
 	float x=10.1, y=20.2;  /*int為整數 x為名稱 x值為10 並開一個記憶體空間給x*/ 
 	float A[10]; /*開10個記憶體位址*/
+	int *p = &x; /* *p->*為指標整數型態 資料的記憶體位址 */
+	int *q = &y;
 	printf("x的值為:%f\n", x);
 	printf("y的值為:%f\n", y);
 	printf("x的位址為:%d\n", &x); /*&x 顯示x記憶體位址*/
 	printf("y的位址為:%d\n", &y);
+	printf("p的值為:%d\n", p);
+	printf("q的值為:%d\n", q);
 	for(int i=0; i<10; i++){
 		printf("陣列A[%d]的位址為:%d\n", i, &A[i]); /*show出 A[0]-A[9]記憶體位址*/
 	}
