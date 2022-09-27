05409052ec2a2fa4989b91cb39a2b136edd33eb3
90polarbear <100282618+90polarbear@users.noreply.github.com>
2022-09-27 19:28:31 +0800

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 int main(void)
 {
-	int A[3][3]; 二維陣列資料
+	int A[3][3]; #二維陣列資料
 	for(int i=0; i<3; i++){
 		for(int j=0; j<3; j++){
 			printf("陣列A[%d][%d]的位址為:%d", i,j, &A[i][j]);		
