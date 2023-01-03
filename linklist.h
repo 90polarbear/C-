dd5964e516b6ceebfa4e76d6fc0f1ee462165844
90polarbear <100282618+90polarbear@users.noreply.github.com>
2023-01-03 20:44:59 +0800

    Update linklist.h

--- linklist.h
+++ linklist.h
@@ -1,4 +1,4 @@
-struct list* createlist(struct list*);
-struct list* Add(struct list*);
+struct list* createlist(struct list*);  #如果在前面主程式main有輸入資料的話在改成struct list*int  char
+struct list* Add(struct list*);         #如果在前面主程式main有輸入資料的話在改成struct list*int  char
 struct list* Delete(struct list*, int);
 struct list* Show(struct list*);
